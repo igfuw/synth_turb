7c90ceeba918c58d4100af7072b2fc9083b0df03
pdziekan <pdziekanp@gmail.com>
2020-12-17 13:16:54 +0100

    plot pair sep test: fill between std dev

--- tests/plot_pair_separation_series.py
+++ tests/plot_pair_separation_series.py
@@ -5,9 +5,10 @@ for sim_type in ["all_waves", "periodic_box"]:
   time = np.loadtxt('build/pair_separation_'+str(sim_type)+'.dat', usecols=0)
   mean_r = np.loadtxt('build/pair_separation_'+str(sim_type)+'.dat', usecols=1)
   sig_r = np.loadtxt('build/pair_separation_'+str(sim_type)+'.dat', usecols=2)
-  plt.errorbar(time, mean_r, sig_r, label=sim_type)
+  plt.plot(time, mean_r, label=sim_type)
+  plt.fill_between(time, mean_r-sig_r, mean_r+sig_r, alpha=0.1)
 
 plt.xlabel('time [s]')
-plt.ylabel('mean separation [m]')
+plt.ylabel('pair separation [m] (mean+-std dev)')
 plt.legend()
 plt.show()
