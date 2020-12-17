6634c1fde845164c7b5247c3c4bc372bc60931d2
pdziekan <pdziekanp@gmail.com>
2020-12-17 12:15:25 +0100

    disp test: output to file

--- tests/dispersion_test.cpp
+++ tests/dispersion_test.cpp
@@ -1,23 +1,28 @@
 #include <synth_turb/SynthTurb3d_periodic_box.hpp>
 #include <synth_turb/SynthTurb3d_all_waves.hpp>
 #include <iostream>
+#include <fstream>
+#include <iomanip>
 #include <random>
 #include <cmath>
 
 #define _NMODES 200
-#define _NWAVES 3
-//#define _NWAVES 50
+#define _NWAVES 5//50
 
 #define NPairs 1000 // number of test particle pairs
 #define InitSep 1 // initial separation [in units of the Kolmogorov length]
 #define LKol 1e-3 // Kolmogorov length [m]
-#define Lmax 100 // integral length [m]
-#define DT 0.1 // [s]
+#define Lmax 1 // integral length [m]
+#define DT 0.01 // [s]
 #define T 10 // [s]
 #define EPS 1e-2 // TKE diss rate [m2/s3]
 
+
+// TODO: 3 tests: all_waves, periodic_box, GA17
+
 int main()
 {
+//  SynthTurb::SynthTurb3d_periodic_box<double, _NMODES, _NWAVES> rm_d(EPS, Lmax, LKol); // eps [m2/s3?], Lmax [m], Lmin[m] (Lmin has no role in the periodic version)
   SynthTurb::SynthTurb3d_all_waves<double, _NMODES, _NWAVES> rm_d(EPS, Lmax, LKol); // eps [m2/s3?], Lmax [m], Lmin[m] (Lmin has no role in the periodic version)
 
   rm_d.generate_random_modes();
@@ -27,17 +32,22 @@ int main()
   std::uniform_real_distribution<double> x_d(0, Lmax),
                                          h_d(-1, std::nextafter(1, std::numeric_limits<double>::max())), // uniform in [-1,1]
                                          th_d(0, std::nextafter(2 * M_PI, std::numeric_limits<double>::max()));  // uniform in [0,2*Pi]
-                                         ;
+                                         
+  std::ofstream ofs ("dispersion_tests.dat", std::ofstream::out);
+  ofs << "#time\t<r>\tsig(r)" << std::endl;
+  ofs << std::setprecision(5) << 0 << "\t" << InitSep*LKol << "\t" << 0 << std::endl;
 
-  double x[NPairs][2][3]; // positions
-  double v[NPairs][2][3]; // velocities
+
+
+  double x[NPairs][2][2][3]; // positions [Npairs][NdropsInPair][n/n+1(pred-corr)][NDims]
+  double v[NPairs][2][2][3]; // velocities
   double r[NPairs]; // separations
   double e[3]; // unit vector
 
   // random position of the first droplet in a pair
   for(int i=0; i<3; ++i)
     for(int p=0; p<NPairs; ++p)
-      x[p][0][i] = x_d(rand_eng);
+      x[p][0][0][i] = x_d(rand_eng);
 
   // positions of the second droplets seprated randomly by InitSep * LKol
   for(int p=0; p<NPairs; ++p)
@@ -49,7 +59,7 @@ int main()
     e[1] = sqrt(1. - h*h) * sin(th);
     e[2] = h;
     for(int i=0; i<3; ++i)
-      x[p][1][i] = x[p][0][i] + e[i] * InitSep * LKol;
+      x[p][1][0][i] = x[p][0][0][i] + e[i] * InitSep * LKol;
   }
 
   double mean_r, sig_r;
@@ -59,23 +69,29 @@ int main()
   for(double t=0; t<=T; t+=DT)
   {
     // update positions
-    // TODO: better integration than Euler
     #pragma omp parallel for
     for(int p=0; p<NPairs; ++p)
     {
       for(int d=0; d<2; ++d)
       {
-        rm_d.calculate_velocity_field(v[p][d][0], v[p][d][1], v[p][d][2], x[p][d], t);
+        // predictor
+        rm_d.calculate_velocity_field(v[p][d][0][0], v[p][d][0][1], v[p][d][0][2], x[p][d][0], t);
         for(int i=0; i<3; ++i)
         {
-          x[p][d][i] += v[p][d][i] * DT;
+          x[p][d][1][i] = x[p][d][0][i] + v[p][d][0][i] * DT; 
+        }
+        // corrector
+        rm_d.calculate_velocity_field(v[p][d][1][0], v[p][d][1][1], v[p][d][1][2], x[p][d][1], t+DT);
+        for(int i=0; i<3; ++i)
+        {
+          x[p][d][0][i] += 0.5 * (v[p][d][0][i] + v[p][d][1][i]) * DT; 
         }
       }
       // calculate separation
       r[p] = sqrt(
-        (x[p][1][0] - x[p][0][0]) * (x[p][1][0] - x[p][0][0]) + 
-        (x[p][1][1] - x[p][0][1]) * (x[p][1][1] - x[p][0][1]) + 
-        (x[p][1][2] - x[p][0][2]) * (x[p][1][2] - x[p][0][2]) 
+        (x[p][1][0][0] - x[p][0][0][0]) * (x[p][1][0][0] - x[p][0][0][0]) + 
+        (x[p][1][0][1] - x[p][0][0][1]) * (x[p][1][0][1] - x[p][0][0][1]) + 
+        (x[p][1][0][2] - x[p][0][0][2]) * (x[p][1][0][2] - x[p][0][0][2]) 
       );
     }
 
@@ -97,6 +113,7 @@ int main()
     }
     sig_r = sqrt(sig_r / NPairs);
 
-    std::cout << "t: " << t << " <r>: " << mean_r << " sig(r): " << sig_r << std::endl;
+    std::cout << "t: " << t+DT << " <r>: " << mean_r << " sig(r): " << sig_r << std::endl;
+    ofs << std::setprecision(5) << t+DT << "\t" << mean_r << "\t" << sig_r << std::endl;
   }
 }
