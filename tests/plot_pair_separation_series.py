c586fc4c210338f899e6547adafc1a61d0779966
pdziekan <pdziekanp@gmail.com>
2022-06-14 16:52:17 +0200

    plotting updates

--- tests/plot_pair_separation_series.py
+++ tests/plot_pair_separation_series.py
@@ -2,11 +2,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 #datadir = '/home/piotr/praca/coal_fluctu_dim/synth_turb_tests/separation_data/DT0.1_L1_EPS0.1/'
-datadir = 'build/'
+datadir = '/home/piotr/praca/coal_fluctu_dim/synth_turb_tests/separation_data/synth_turb_separate_code/Feb_2022/multiwave_nwave50_nmode10_and_others/'
+#datadir = 'build/'
+
+labels = {
+  "all_waves" : "non-periodic",
+  "periodic_box" : "periodic",
+  "periodic_box_multiwave" : "periodic"
+}
 
 for eps in [0.01,0.1,1,10,100,1000]:
   plt.clf()
-  for sim_type in ["all_waves", "periodic_box", "periodic_box_multiwave", "GA17"]:
+  #for sim_type in ["all_waves", "periodic_box", "periodic_box_multiwave", "GA17"]:
+  for sim_type in ["all_waves", "periodic_box"]:
     simname = str(sim_type)+'_EPS'+str(eps)
     time = np.loadtxt(str(datadir)+'pair_separation_'  +simname+'.dat', usecols=0)
     mean_r = np.loadtxt(str(datadir)+'pair_separation_'+simname+'.dat', usecols=1)
@@ -16,7 +24,7 @@ for eps in [0.01,0.1,1,10,100,1000]:
   #  mean_r /= 1e-3
   #  sig_r /= 1e-3
   
-    plt.plot(time, mean_r, label=sim_type)
+    plt.plot(time, mean_r, label=labels[sim_type])
     plt.fill_between(time, mean_r-sig_r, mean_r+sig_r, alpha=0.1)
   
   plt.xlabel('time [s]')
